0dc501d7b4b9510c032ed4a7dbd9365faed60382
ankitpatel005 <108638996+ankitpatel005@users.noreply.github.com>
2023-04-03 19:24:52 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,9 +9,9 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '19455849'))
-    API_HASH = str(getenv('API_HASH', '71498d69fad014add7e9f717bfde4b79'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '5451383761:AAGYe32Dv1Qe2NBvogzclNzCx-7Geup9pYc'))
+    API_ID = int(getenv('API_ID', '10079689'))
+    API_HASH = str(getenv('API_HASH', '8e402fe63a69576b82cda6daf1ab617f'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '5529559409:AAGjVz-X9m-SZ9bS8GsZS-H1r32Jxbq6FwU'))
     name = str(getenv('name', 'bottest67bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
@@ -19,10 +19,10 @@ class Var(object):
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "905710386").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1729415252").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'sahid_malik'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'I_m_available_24hrs'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
